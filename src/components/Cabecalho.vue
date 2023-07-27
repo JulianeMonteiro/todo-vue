62d2b1b26e798de963b9ac725e0fae039b6f547a
JulianeMonteiro <juliane.a.monteiro@hotmail.com>
2023-07-27 00:30:21 -0300

    Componetizando a aplicação

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps(["tarefasPendentes"]);
+</script>
+<template>
+  <header v-el class="p-5 mb-4 mt-4 bg-light rounded-3">
+    <h1>Minhas tarefas</h1>
+    <p v-if="props.tarefasPendentes == 1">
+      Você possui {{ props.tarefasPendentes }} tarefa pendente
+    </p>
+
+    <p v-else-if="props.tarefasPendentes > 1">
+      Você possui {{ props.tarefasPendentes }} tarefas pendentes
+    </p>
+
+    <p v-else>Parabéns! Você não possui tarefas pendentes!</p>
+  </header>
+</template>
---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps(["tarefas"]);
+</script>
+
+<template>
+  <ul v-if="props.tarefas.length > 0" class="list-group mt-4">
+    <li class="list-group-item" v-for="tarefa in props.tarefas">
+      <input
+        @change="(evento) => (tarefa.finalizada = evento.target.checked)"
+        :checked="tarefa.finalizada"
+        :id="tarefa.titulo"
+        type="checkbox"
+      />
+      <label
+        :class="{ done: tarefa.finalizada }"
+        class="ms-3"
+        :for="tarefa.titulo"
+      >
+        {{ tarefa.titulo }}
+      </label>
+    </li>
+  </ul>
+  <p v-else class="mt-2 ms-3 warning-text">Não existem tarefas Pendentes</p>
+</template>
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+
+.warning-text {
+  color: #918f8e;
+}
+</style>
